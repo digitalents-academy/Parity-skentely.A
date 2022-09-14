885a8b1b3483beea23e3f51af720a490a2bfb700
hnnav <57915266+hnnav@users.noreply.github.com>
2022-09-14 14:46:45 +0300

    nav dropdown hidden on refresh

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function showNav() {
     const navDropDown = document.querySelector(".nav-dropdown");
     const bottomPage = document.querySelector(".bottom-page")
-    if (navDropDown.style.display === "none") {
+    let displayStatus = navDropDown.style.display
+    
+    if (displayStatus === "none" || displayStatus === "") {
       navDropDown.style.display = "block";
       bottomPage.style.display = "none";
     } else {

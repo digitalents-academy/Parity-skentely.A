b6e731ce8a218179af0cd08d2005b798ab12fa5d
hnnav <57915266+hnnav@users.noreply.github.com>
2022-09-22 12:13:29 +0300

    blur overlay

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function showNav() {
     const navDropDown = document.querySelector(".nav-dropdown");
-    const bottomPage = document.querySelector(".bottom-page")
+    const colorOverlay = document.querySelector(".color-overlay")
     let displayStatus = navDropDown.style.display
     
     if (displayStatus === "none" || displayStatus === "") {
       navDropDown.style.display = "block";
-      bottomPage.style.display = "none";
+      colorOverlay.style.display = "block";
     } else {
       navDropDown.style.display = "none";
-      bottomPage.style.display = "block";
+      colorOverlay.style.display = "none";
     }
 }
+
+// Original:
+
+/* function showNav() {
+  const navDropDown = document.querySelector(".nav-dropdown");
+  const bottomPage = document.querySelector(".bottom-page")
+  let displayStatus = navDropDown.style.display
+  
+  if (displayStatus === "none" || displayStatus === "") {
+    navDropDown.style.display = "block";
+    bottomPage.style.display = "none";
+  } else {
+    navDropDown.style.display = "none";
+    bottomPage.style.display = "block";
+  }
+} */

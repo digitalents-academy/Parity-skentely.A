9f3b2892678428837024356bfe87ff5391c7c199
Aisha-Amalia <79995663+Aisha-Amalia@users.noreply.github.com>
2022-09-26 10:12:02 +0300

    final revison2

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function showNav() {
+  const navDropDown = document.querySelector(".nav-dropdown");
+  const colorOverlay = document.querySelector(".color-overlay")
+  let displayStatus = navDropDown.style.display
+  
+  if (displayStatus === "none" || displayStatus === "") {
+    navDropDown.style.display = "block";
+    colorOverlay.style.display = "block";
+  } else {
+    navDropDown.style.display = "none";
+    colorOverlay.style.display = "none";
+  }
+}
